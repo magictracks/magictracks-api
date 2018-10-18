a4e677e59345cfb1782879eb02dbe486d46a43b8
joeyklee <j@jk-lee.com>
2018-10-18 13:37:16 -0400

    creating explicit api endpoints

--- src/services/resources/resources.service.js
+++ src/services/resources/resources.service.js
@@ -1,23 +1,100 @@
 // Initializes the `resources` service on path `/resources`
-const createService = require('feathers-mongoose');
-const createModel = require('../../models/resources.model');
-const hooks = require('./resources.hooks');
+const createService = require("feathers-mongoose");
+const createModel = require("../../models/resources.model");
+const hooks = require("./resources.hooks");
 
-module.exports = function (app) {
+module.exports = function(app) {
   const Model = createModel(app);
-  const paginate = app.get('paginate');
+  const paginate = app.get("paginate");
 
   const options = {
     Model,
     paginate
   };
 
-  // Initialize our service with any options it requires
-  app.use('/resources', createService(options));
+  /**
+   * @@ ROUTE: /resources/all 
+   */
+  app.use("/resources/all", {
+    /**
+     * GET
+     * @param {*} params 
+     * returns an ARRAY of the sections
+     */
+    async find(params) {
+      const result = await Model.find({});
+      return result;
+    }
+  });
+  // app.service('/sections/:id/test').hooks(hooks);  
 
+  /**
+   * @@ ROUTE: /resources/id/:id
+   */
+  app.use("/resources/id/:id", {
+    /**
+     * FIND
+     * @param {*} params 
+     * finds the data by id and returns it
+     */
+    async find(params){
+      const { id } = params.route;
+      const result = await Model.findOne({ _id: id });
+      return result;
+    },
+    /**
+     * PATCH
+     * @param {*} _id 
+     * @param {*} data 
+     * @param {*} params 
+     * finds the data by id and returns the updated data
+     */
+    async patch(_id, data, params) {
+      const { id } = params.route;
+      const result = await Model.findByIdAndUpdate(id, {$set:data}, {new:true});
+      return result;
+    },
+
+    /**
+     * REMOVE
+     * @param {*} _id 
+     * @param {*} params 
+     */
+    async remove(_id, params){
+        const {id} = params.route;
+        const result = await Model.deleteOne({_id:id});
+        console.log(id, result)
+
+        return {message: "resource removed!"}
+      }
+    });
+
+   /**
+   * @@ ROUTE: /resources/add
+   */
+   app.use("/resources/add", {
+     /**
+      * CREATE
+      * @param {*} data 
+      * @param {*} params 
+      * send in a json object of data and get back the submitted object
+      */
+      async create(data, params) {
+        const result = await Model.create(data);
+        return result;
+      }
+   });
+   // app.service('/sections/:id/test').hooks(hooks);  
+
+
+
+
+
+  // Initialize our service with any options it requires
+  app.use("/resources", createService(options));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('resources');
+  const service = app.service("resources");
 
   service.hooks(hooks);
 };
--- src/services/sections/sections.service.js
+++ src/services/sections/sections.service.js
@@ -14,11 +14,14 @@ module.exports = function (app) {
 
 
   /**
-  * GET
   * /sections/all
-  * returns an ARRAY of the sections
   */
   app.use('/sections/all', {
+    /**
+     * FIND()
+     * @param {*} params 
+     * returns an ARRAY of the sections
+     */
     async find(params){
       const result = await Model.find({}).populate('resources').exec();
       return result;
@@ -27,20 +30,59 @@ module.exports = function (app) {
   // app.service('/sections/:id/test').hooks(hooks);
 
   /**
-  * GET
-  * /sections/id/:id
-  * returns a JSON of the requested section with the ID specified
+  * @@ Route: /sections/id/:id 
   */
   app.use('/sections/id/:id', {
+    /**
+     * FIND()
+     * @param {*} params 
+     * returns a JSON of the requested section with the ID specified
+     */
     async find(params){
       const {id} = params.route;
-
       const result = await Model.findOne({_id:id}).populate('resources').exec();
+      return result;
+    },
+    /**
+     * PATCH()
+     * @param {*} params 
+     * returns a json of the updated values. 
+     * NOTE: if you want to $push or remove values from an array, see /sections/<action>/:property/id/:id
+     */
+    async patch(_id, data, params){
+      const {id} = params.route;
+      const result = await Model.findByIdAndUpdate(id, 
+        {$set:data}, 
+        {new:true}).populate('resources').exec();
+
       return result;
     }
   });
   // app.service('/sections/:id/test').hooks(hooks);
 
+  app.use('/sections/id/:id/appendOne/:property', {
+    async patch(_id, data, params){
+      const {id} = params.route;
+      const {property} = params.route;
+      const result = await Model.findByIdAndUpdate(id, 
+        {$push:{[property]: data.payload }}, 
+        {new:true}).populate('resources').exec();
+      
+      return result;
+    }
+  });
+
+  app.use('/sections/id/:id/removeOne/:property', {
+    async patch(_id, data, params){
+      const {id} = params.route;
+      const {property} = params.route;
+      const result = await Model.findByIdAndUpdate(id, 
+        {$pull:{[property]: data.payload }}, 
+        {new:true}).populate('resources').exec();
+      
+      return result;
+    }
+  });
 
   // Initialize our service with any options it requires
   app.use('/sections', createService(options));
