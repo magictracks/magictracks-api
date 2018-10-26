2cca54235bf27ba7e5bd7666cbd89920a2f39503
joeyklee <j@jk-lee.com>
2018-10-26 10:36:27 -0400

    added data and updated import script

--- scripts/importReal2db.js
+++ scripts/importReal2db.js
@@ -2,20 +2,18 @@ const fs = require('fs');
 const rp = require('request-promise');
 const Q = require('q');
 
-// const myResources = JSON.parse(fs.readFileSync('data/resources-exported.json'));
+const myResources = JSON.parse(fs.readFileSync('data/resources-exported.json'));
 const mySections = JSON.parse(fs.readFileSync('data/sections-exported.json'));
 const myPlaylists = JSON.parse(fs.readFileSync('data/playlists-exported.json'));
 // const myTags = JSON.parse(fs.readFileSync('data/tags.json'));
 
 if (process.argv[2] == 'resources') {
-  // TODO
-  //   postMany2Mongo(myResources, 'http://127.0.0.1:3030/resources/addJSON');
-  console.log("TBD");
+  postMany2Mongo(myResources, 'http://127.0.0.1:3030/resources/');
 } else if (process.argv[2] == 'sections') {
   postMany2Mongo(mySections, 'http://127.0.0.1:3030/sections/addJSON');
 } else if (process.argv[2] == 'playlists') {
   postMany2Mongo(myPlaylists, 'http://127.0.0.1:3030/playlists/addJSON');
-} 
+}
 
 function postOne2Mongo(payload, _url) {
   let options = {
