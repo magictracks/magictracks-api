e1647e75882fb9b3525341a007f35ae78d7a1016
joeyklee <j@jk-lee.com>
2018-10-26 16:32:44 -0400

    refactored playlist endpoints more generic

--- src/services/playlists/playlists.service.js
+++ src/services/playlists/playlists.service.js
@@ -62,16 +62,50 @@ module.exports = function (app) {
             return err;
           }
         },
-        // patch one
+        // put one
+
+        /**
+         * 
+         * @param {*} _id 
+         * @param {*} data 
+         * [
+         *     {"op":"set", "path":"title", "value":"Learning with Dan is my Favorite Thing"}, 
+         *     {"op":"set", "path":"description",  "value":"don't you want to learn with Dan?"}, 
+         *     {"op":"push", "path":"sections", "value":"5bd366ac399838e5d063aa2c"}
+         * ]
+         * @param {*} params 
+         */
         async patch(_id, data, params) {
           try {
             const {
               id
             } = params.route;
-            const result = await Model.findByIdAndUpdate(
-                id, {
-                  $set: data
-                }, {
+
+            let updateProps = {
+              "$set": {},
+              "$push": {},
+              "$pull": {}
+            }
+
+            data.forEach( item => {
+              if(item.op === "set"){
+                updateProps["$set"] = Object.assign({[item.path]: item.value },  updateProps["$set"]);
+              } else if (item.op === "push"){
+                updateProps["$push"] = Object.assign({[item.path]: item.value },  updateProps["$push"]);
+              } else if (item.op === "pull"){
+                updateProps["$pull"] = Object.assign({[item.path]: item.value },  updateProps["$pull"]);
+              }
+            });
+
+            Object.keys(updateProps).forEach( (key) => {
+              if( Object.keys(updateProps[key]).length < 1 ){
+                delete updateProps[key]
+              }
+            });
+
+          
+            const result = await Model.findOneAndUpdate(
+                id, updateProps, {
                   new: true
                 }
               )
@@ -172,96 +206,6 @@ module.exports = function (app) {
         }
       };
 
-
-      this.pushItem = {
-        /**
-         * @@ ROUTE: /playlists/id/:id/appendOne/:property
-         * :property == resources, tags, users, etc
-         * TODO: make sure to sanitize out any Users, etc
-         */
-        /**
-         * Patch()
-         * @param {*} _id
-         * @param {*} data
-         * @param {*} params
-         * using the property specified in the URL, push one value from the payload to that array
-         */
-        async patch(_id, data, params) {
-          try {
-            const {
-              id
-            } = params.route;
-            const {
-              property
-            } = params.route;
-            const result = await Model.findByIdAndUpdate(
-                id, {
-                  $push: {
-                    [property]: data.payload
-                  }
-                }, {
-                  new: true
-                }
-              )
-              .populate({
-                path: 'sections',
-                model: 'sections',
-                populate: {
-                  path: 'resources',
-                  model: 'resources'
-                }
-              })
-              .exec();
-
-            return result;
-          } catch (err) {
-            return err;
-          }
-        }
-      };
-
-      this.pullItem = {
-        /**
-         * Patch()
-         * @param {*} _id
-         * @param {*} data
-         * @param {*} params
-         * using the property specified in the URL, pull the value specified from that array
-         */
-        async patch(_id, data, params) {
-          try {
-            const {
-              id
-            } = params.route;
-            const {
-              property
-            } = params.route;
-            const result = await Model.findByIdAndUpdate(
-                id, {
-                  $pull: {
-                    [property]: data.payload
-                  }
-                }, {
-                  new: true
-                }
-              )
-              .populate({
-                path: 'sections',
-                model: 'sections',
-                populate: {
-                  path: 'resources',
-                  model: 'resources'
-                }
-              })
-              .exec();
-
-            return result;
-          } catch (err) {
-            return err;
-          }
-        }
-      };
-
     }; // end constructor
   }; // end controllers
 
@@ -270,9 +214,7 @@ module.exports = function (app) {
   app.use('/playlists', controllers.all);
   app.use('/playlists/id/:id', controllers.byId);
   app.use('/playlists/addJSON', controllers.addJSON);
-  app.use('/playlists/id/:id/pushItem/:property', controllers.pushItem);
-  app.use('/playlists/id/:id/pullItem/:property', controllers.pullItem);
-
+  
 
   // Initialize our service with any options it requires
   app.use('/playlists', createService(options));
@@ -282,275 +224,3 @@ module.exports = function (app) {
 
   service.hooks(hooks);
 };
-
-
-// /**
-//    * /playlists/all
-//    */
-//   app.use('/playlists/all', {
-//     /**
-//      * FIND()
-//      * @param {*} params
-//      * returns an ARRAY of the playlists
-//      */
-//     async find(params) {
-//       const result = await Model.find({})
-//         .populate({
-//           path: 'sections',
-//           model: 'sections',
-//           populate: {
-//             path: 'resources',
-//             model: 'resources'
-//           }
-//         })
-//         .exec();
-//       return result;
-//     }
-//   });
-//   // app.service('/playlists/:id/test').hooks(hooks);
-
-//   /**
-//    * @@ Route: /playlists/id/:id
-//    */
-//   app.use('/playlists/id/:id', {
-//     /**
-//      * FIND()
-//      * @param {*} params
-//      * returns a JSON of the requested section with the ID specified
-//      */
-//     async find(params) {
-//       try {
-//         const {
-//           id
-//         } = params.route;
-//         console.log(id);
-//         const result = await Model.findOne({
-//             _id: id
-//           })
-//           .populate({
-//             path: 'sections',
-//             model: 'sections',
-//             populate: {
-//               path: 'resources',
-//               model: 'resources'
-//             }
-//           })
-//           .exec();
-//         return result;
-//       } catch (err) {
-//         return err;
-//       }
-//     },
-//     /**
-//      * PATCH()
-//      * @param {*} params
-//      * returns a json of the updated values.
-//      * NOTE: if you want to $push or remove values from an array, see /playlists/<action>/:property/id/:id
-//      */
-//     async patch(_id, data, params) {
-//       try {
-//         const {
-//           id
-//         } = params.route;
-//         const result = await Model.findByIdAndUpdate(
-//             id, {
-//               $set: data
-//             }, {
-//               new: true
-//             }
-//           )
-//           .populate({
-//             path: 'sections',
-//             model: 'sections',
-//             populate: {
-//               path: 'resources',
-//               model: 'resources'
-//             }
-//           })
-//           .exec();
-
-//         return result;
-//       } catch (err) {
-//         return err;
-//       }
-//     },
-
-//     /**
-//      * REMOVE
-//      * @param {*} _id
-//      * @param {*} params
-//      * using the id provided, remove the specified section from the database
-//      */
-//     async remove(_id, params) {
-//       try {
-//         const {
-//           id
-//         } = params.route;
-//         const result = await Model.deleteOne({
-//           _id: id
-//         });
-//         return {
-//           message: 'playlist removed!'
-//         };
-//       } catch (err) {
-//         return {
-//           message: err
-//         };
-//       }
-//     }
-//   });
-//   // app.service('/playlists/:id/test').hooks(hooks);
-
-//   /**
-//    * @@ ROUTE: /playlists/id/:id/appendOne/:property
-//    * :property == resources, tags, users, etc
-//    * TODO: make sure to sanitize out any Users, etc
-//    */
-//   app.use('/playlists/id/:id/appendOne/:property', {
-//     /**
-//      * Patch()
-//      * @param {*} _id
-//      * @param {*} data
-//      * @param {*} params
-//      * using the property specified in the URL, push one value from the payload to that array
-//      */
-//     async patch(_id, data, params) {
-//       try {
-//         const {
-//           id
-//         } = params.route;
-//         const {
-//           property
-//         } = params.route;
-//         const result = await Model.findByIdAndUpdate(
-//             id, {
-//               $push: {
-//                 [property]: data.payload
-//               }
-//             }, {
-//               new: true
-//             }
-//           )
-//           .populate({
-//             path: 'sections',
-//             model: 'sections',
-//             populate: {
-//               path: 'resources',
-//               model: 'resources'
-//             }
-//           })
-//           .exec();
-
-//         return result;
-//       } catch (err) {
-//         return err;
-//       }
-//     }
-//   });
-
-//   /**
-//    * @@ ROUTE: /playlists/addJSON
-//    * :property == resources, tags, users, etc
-//    * TODO: make sure to sanitize out any Users, etc
-//    */
-//   app.use('/playlists/addJSON', {
-//     /**
-//      * create()
-//      * @param {*} _id
-//      * @param {*} data
-//      * @param {*} params
-//      * Get a JSON of a playlist > 
-//      * For each section > save all the resources in the db and return their ids > save the section >
-//      * Return a list of section ids > store them to the playlist.sections > save the playlist
-//      */
-//     async create(data, params) {
-//       try {
-//         const PlaylistModel = Model.model('playlists');
-//         const SectionModel = Model.model('sections');
-//         const ResourceModel = Model.model('resources');
-
-//         let playlist;
-//         // create a playlist model which gives us a place to stuff in our ids
-//         playlist = new PlaylistModel(data);
-
-//         // return an array of section ids after saving them with their respective resources
-//         playlist.sections = await Promise.all(data.sections.map(async (section) => {
-//           try {
-//             // get the section model
-//             let sect = new SectionModel(section);
-//             // get an array of the ids from the saved resources
-//             // save the resource ids into the sections.resources objectId array 
-//             sect.resources = await Promise.all(section.resources.map(async (resource) => {
-//               try {
-//                 let rsc = await new ResourceModel(resource).save();
-//                 return rsc._id;
-//               } catch (innerErr2) {
-//                 return innerErr2;
-//               }
-
-//             }))
-//             // save each section
-//             await sect.save();
-//             return sect._id;
-//           } catch (innerErr1) {
-//             return innerErr1;
-//           }
-//         }));
-
-//         // add the section ids to the playlist
-//         playlist = await playlist.save();
-//         // return the populated item
-//         return Model.findById(playlist._id).populate({
-//           path: "sections",
-//           model: "sections",
-//           populate: {
-//             path: "resources",
-//             model: "resources"
-//           }
-//         }).exec();
-//       } catch (err) {
-//         return err;
-//       }
-//     }
-//   });
-
-//   /**
-//    * @@ ROUTE: /playlists/id/:id/removeOne/:property
-//    */
-//   app.use('/playlists/id/:id/removeOne/:property', {
-//     /**
-//      * Patch()
-//      * @param {*} _id
-//      * @param {*} data
-//      * @param {*} params
-//      * using the property specified in the URL, pull the value specified from that array
-//      */
-//     async patch(_id, data, params) {
-//       try {
-//         const {id} = params.route;
-//         const {property} = params.route;
-//         const result = await Model.findByIdAndUpdate(
-//             id, {
-//               $pull: {
-//                 [property]: data.payload
-//               }
-//             }, {
-//               new: true
-//             }
-//           )
-//           .populate({
-//             path: 'sections',
-//             model: 'sections',
-//             populate: {
-//               path: 'resources',
-//               model: 'resources'
-//             }
-//           })
-//           .exec();
-
-//         return result;
-//       } catch (err) {
-//         return err;
-//       }
-//     }
-//   });
