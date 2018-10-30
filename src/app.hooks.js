3d79665822c8db588f0783d28a606b63594594a3
joeyklee <j@jk-lee.com>
2018-10-30 13:17:44 -0400

    rm casl

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -1,20 +1,9 @@
 // Application hooks that run for every service
 const log = require('./hooks/log');
-const {
-  when
-} = require('feathers-hooks-common');
-const authorize = require('./hooks/abilities');
-const authenticate = require('./hooks/authenticate');
 
 module.exports = {
   before: {
-    all: [log(),
-      when(
-        hook => hook.params.provider && `/${hook.path}` !== hook.app.get('authentication').path,
-        authenticate,
-        authorize()
-      )
-    ],
+    all: [log()],
     find: [],
     get: [],
     create: [],
